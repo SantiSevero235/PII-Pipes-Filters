45364d6f4a20c41dde63b8f9c1e3fb3a0e133280
[Santiago Severo] <[santiagosevero28@gmail.com]>
2022-06-04 02:24:39 -0300

    Ejercicio 1 hecho y NET 6.0

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,19 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            //Ejercicio 1
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture(@"luke.jpg");
+            IFilter negativefilter = new FilterNegative();
+            IFilter greyscalefilter = new FilterGreyscale();
+            IPipe pipenull = new PipeNull();
+            IPipe pipe = new PipeSerial(negativefilter,pipenull);
+            IPicture image1 = pipe.Send(picture);
+            provider.SavePicture(image1,@"lukecopy.jpg");
+            IPipe pipe2 = new PipeSerial(greyscalefilter,pipe);
+            IPicture picture2 = provider.GetPicture(@"beer.jpg");
+            IPicture image2 = pipe2.Send(picture2);
+            provider.SavePicture(image2,@"beercopy.jpg");
         }
     }
 }
